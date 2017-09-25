6bc3ece0c5dfe5baa9596c67f743df362fbff550
Mikolaj Kacprzak <mikolaj.kacprzak@skybettingandgaming.com>
2017-09-25 15:01:44 +0100

    [sinewave] rainbow color loopin

--- patterns/sinewave.py
+++ patterns/sinewave.py
@@ -2,6 +2,70 @@ import cubehelper
 import random
 import math
 
+'''def compo(pattArray,angle):
+    for num in pattArray["high"]:
+        if (angle >= num[0]) and (angle <= num[1]):
+            comp = 1.0
+
+    for num in pattArray["low"]:
+        if (angle >= num[0]) and (angle <= num[1]):
+            comp = 0.0
+
+    for num in pattArray["up"]:
+        if (angle >= num[0]) and (angle <= num[1]):
+            comp = (angle - num[0])/(num[1]-num[0]+0.0)
+
+    for num in pattArray["down"]:
+        if (angle >= num[0]) and (angle <= num[1]):
+            comp = (num[1] - angle)/(num[1]-num[0]+0.0)
+
+    return comp'''
+
+def rainbow(iter,step):
+    def compo(pattArray,angle):
+        for num in pattArray["high"]:
+            if (angle >= num[0]) and (angle <= num[1]):
+                comp = 1.0
+
+        for num in pattArray["low"]:
+            if (angle >= num[0]) and (angle <= num[1]):
+                comp = 0.0
+
+        for num in pattArray["up"]:
+            if (angle >= num[0]) and (angle <= num[1]):
+                comp = (angle - num[0])/(num[1]-num[0]+0.0)
+
+        for num in pattArray["down"]:
+            if (angle >= num[0]) and (angle <= num[1]):
+                comp = (num[1] - angle)/(num[1]-num[0]+0.0)
+
+        return comp
+
+    angle = (iter*step)%360
+
+    b = {
+    "high": [[180, 299]],
+    "low":  [[0, 119]],
+    "up": [[120, 179]],
+    "down": [[300, 359]]
+    }
+
+    r = { 
+    "high": [[300, 359], [0, 59]], 
+    "low": [[120, 239]], 
+    "up": [[240, 299]], 
+    "down": [[60, 119]] 
+    }
+
+    g = {
+    "high": [[60, 179]],
+    "low": [[240, 359]],
+    "up": [[0, 59]],
+    "down": [[180, 239]]
+    }
+
+    return (compo(r,angle), compo(g,angle), compo(b,angle))
+
 class Pattern(object):
     def init(self):
         self.iter = 1
@@ -9,8 +73,7 @@ class Pattern(object):
         self.sinwidth = 0.4
         self.color = cubehelper.random_color()
         self.prevColor = cubehelper.random_color()
-        self.inv = 1
-        self.widthincrementor = 0.01
+        
         return 1.0/10
 
     def tick(self):
@@ -18,12 +81,11 @@ class Pattern(object):
         color = cubehelper.mix_color(self.prevColor, self.color, (math.sin(self.sinwidth*self.iter) + 1 )/2)
         ran = random.randrange(100)
 
-        if ran < 3:
-            self.inv = -self.inv
         for x in range(0, self.cube.size):
             for y in range(0, self.cube.size):
-                z = math.floor(math.sin(self.sinwidth*(self.iter+(self.inv*(x+y))))*3.5+4)
-                self.cube.set_pixel((x, y, z), color)
+                z = math.floor(math.sin(self.sinwidth*(self.iter+(x+y)))*3.5+4)
+                self.cube.set_pixel((x, y, z), rainbow(self.iter, 10))
+
         self.iter += self.delta
         if (math.sin(self.sinwidth*self.iter)+1)/2 < 0.1:
             self.prevColor = self.color
